18343df8c4d55935a45067e2adebedc63424ad95
henrykironde <henrykironde@gmail.com>
2025-02-01 03:52:34 -0500

    Improve docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,3 +5,14 @@ project = "numFOCUS Project Infrastructure"
 release = ""
 copyright = f"{datetime.now().year}, {author}"  # NOQA: A001 DTZ005
 html_theme = "pydata_sphinx_theme"
+
+
+html_logo = "_static/NumFocus_LRG.png"
+html_favicon = "_static/NumFocus_LRG.png"
+html_theme_options = {
+    "logo": {
+    	"text": "Project Infrastructure",
+        "image_light": "_static/NumFocus_LRG.png",  # For light mode
+        "image_dark": "_static/NumFocus_LRG.png",   # For dark mode
+    }
+}
